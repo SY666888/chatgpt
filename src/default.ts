0a709cdfcffb773e7e36cfd2f9b823d09ceb9d90
zsysem <1291679702@qq.com>
2023-03-14 17:14:28 +0800

    '修改文字说明'

--- src/default.ts
+++ src/default.ts
@@ -7,7 +7,7 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索角色预设。
+- Shift + Enter 换行。开头输入 / 或者 空格 搜索角色预设。
 - 如果出现错误，则是后台欠费，请过一段时期再使用！
  — 基于[chatgpt-vercel](https://github.com/ourongxing/chatgpt-vercel)。
 `
