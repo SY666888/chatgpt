c8f7bd7b3d04d56261ed2715e5ec9c63dcf46a3c
SY666888 <71245215+SY666888@users.noreply.github.com>
2023-03-21 16:58:22 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -9,5 +9,5 @@ export const defaultSetting = {
 export const defaultMessage = `
 - Shift + Enter 换行。开头输入 / 或者 空格 搜索角色预设。
 - 如果出现错误，则是后台欠费，请过一段时期再使用！
- — 基于[chatgpt-vercel](https://github.com/ourongxing/chatgpt-vercel)。
+ — 基于[ chatgpt-vercel ](https://github.com/ourongxing/chatgpt-vercel)。
 `
